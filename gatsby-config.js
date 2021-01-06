ef3ed2d9db9ab41ae03e3509954f675ab8f3a749
ActarusC <cadieuxleblanc@gmail.com>
2021-01-05 22:37:23 -0500

    Ajout de Markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Site de test`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -42,6 +42,14 @@ module.exports = {
         path: `${__dirname}/src/data`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    "gatsby-transformer-remark"
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
---
+++ src/markdown-pages/post-1.md
@@ -0,0 +1,9 @@
+---
+slug: "/doc"
+date: "2019-05-04"
+title: "Documentation"
+---
+J'essaie du markdown
+
+* ok
+
