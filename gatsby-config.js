3b407d1ba9601ae963a6c03036cddf6c1a07778e
ActarusC <cadieuxleblanc@gmail.com>
2021-01-09 09:54:15 -0500

    poolers fonctionne avec json panda, tri par score

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `pool-de-hockey`,
+        short_name: `pool`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `graveyard`,
+        name: `data`,
         path: `${__dirname}/src/data`,
       },
     },
