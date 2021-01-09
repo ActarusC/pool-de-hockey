9fa11070b6c7015dd12d7f6e948daf7bc542033c
ActarusC <cadieuxleblanc@gmail.com>
2021-01-09 10:09:48 -0500

    la doc est maintenant en md dans le code

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,14 +42,6 @@ module.exports = {
         path: `${__dirname}/src/data`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`,
-      },
-    },
-    "gatsby-transformer-remark"
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
