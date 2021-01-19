8e3bd41da96e8cc121aae2e5d91adfd16f25f580
ActarusC <cadieuxleblanc@gmail.com>
2021-01-19 16:15:06 -0500

    La réserve est visiable.

--- src/templates/pooler.js
+++ src/templates/pooler.js
@@ -5,10 +5,8 @@ import { Link } from "gatsby"
 
 const alignements = require("../data/alignements.json").data
 
-function filtrerAlignement(align, idPooler) {
-  const alignF = align.filter((unA => {
-    return (unA.idPooler === idPooler)
-  }))
+function filtrerAlignement(align, idPooler, statusA) {
+  const alignF = align.filter(unA => (unA.idPooler == idPooler && unA.statutJoueur == statusA))
   return alignF
 }
 
@@ -17,16 +15,19 @@ const Pooler = ({ pageContext }) => (
     <SEO title={pageContext.Equipe} />
     <h1>{pageContext.Equipe}</h1>
     <p>Dirigée par <b>{pageContext.nom}</b> </p>
+    <h2>Alignement actif</h2>
     <table>
       <thead>
         <tr>
           <th>Joueur</th>
           <th>Position</th>
           <th>Score</th>
+          <th>Salaire</th>
+          <th>Date d'ajout</th>
         </tr>
       </thead>
       <tbody>
-        {filtrerAlignement(alignements, pageContext.Id).map(unJoueur => (
+        {filtrerAlignement(alignements, pageContext.Id, "Alignement").map(unJoueur => (
           <tr key={unJoueur.idAlignement}>
             <td>
               <Link to={"/joueur/" + unJoueur.idNHL}>
@@ -34,6 +35,37 @@ const Pooler = ({ pageContext }) => (
               </Link></td>
             <td>{unJoueur.position} </td>
             <td>{unJoueur.pointsActuels} </td>
+            <td>{unJoueur.salaireActuel} </td>
+            <td>{unJoueur.dateDebut} </td>
+          </tr>
+
+        ))
+        }
+      </tbody>
+    </table>
+
+    <h2>Réserve</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Joueur</th>
+          <th>Position</th>
+          <th>Score</th>
+          <th>Salaire</th>
+          <th>Date d'ajout</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filtrerAlignement(alignements, pageContext.Id, "R\u00e9serve").map(unJoueur => (
+          <tr key={unJoueur.idAlignement}>
+            <td>
+              <Link to={"/joueur/" + unJoueur.idNHL}>
+                {unJoueur.prenomJoueur + " " + unJoueur.nomJoueur}
+              </Link></td>
+            <td>{unJoueur.position} </td>
+            <td>{unJoueur.pointsActuels} </td>
+            <td>{unJoueur.salaireActuel} </td>
+            <td>{unJoueur.dateDebut} </td>
           </tr>
 
         ))
