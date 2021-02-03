6a0f814b00dcfa572ed3d68e3d4478c8570adc3b
ActarusC <cadieuxleblanc@gmail.com>
2021-02-02 20:23:45 -0500

    ajout section Espoirs ds alignement

--- src/templates/pooler.js
+++ src/templates/pooler.js
@@ -10,74 +10,78 @@ function filtrerAlignement(align, idPooler, statusA) {
   return alignF
 }
 
+function enteteAlignement() {
+  return(
+    <thead>
+    <tr>
+      <th>Joueur</th>
+      <th>Position</th>
+      <th>PJ</th>
+      <th>Buts</th>
+      <th>Aides</th>
+      <th>Score</th>
+      <th>Salaire</th>
+      <th>Date d'ajout</th>
+    </tr>
+  </thead>
+  )
+}
+
+function bodyAlignement(idPooler, statusA){
+  return (
+    <tbody>
+    {filtrerAlignement(alignements, idPooler, statusA).map(unJoueur => (
+      <tr key={unJoueur.idAlignement}>
+        <td>
+          <Link to={"/joueur/" + unJoueur.idNHL}>
+            {unJoueur.prenomJoueur + " " + unJoueur.nomJoueur}
+          </Link></td>
+        <td>{unJoueur.position} </td>
+        <td>{unJoueur.pjActuels} </td>
+        <td>{unJoueur.butsActuels} </td>
+        <td>{unJoueur.assistActuels} </td>
+        <td>{unJoueur.pointsActuels} </td>
+        <td>{(unJoueur.salaireActuel / 10**6).toFixed(2)  } M$</td>
+        <td>{unJoueur.dateDebut} </td>
+      </tr>
+
+    ))
+    }
+  </tbody>
+  )
+}
+
+function unTypeAlignement(idPooler, statusA, titreA) {
+  return (
+    <div>
+      <h2>{titreA}</h2>
+      <table>
+        {enteteAlignement()}
+        {bodyAlignement(idPooler, statusA)}
+     </table>
+    </div>
+  )
+}
+
+function typesAlignements(idPooler) {
+  return(
+    <div>
+      { unTypeAlignement(idPooler, "Alignement", "Alignement actif") }
+      { unTypeAlignement(idPooler, "R\u00e9serve", "R\u00e9serve") }
+      { unTypeAlignement(idPooler, "Espoir", "Espoirs") }
+      
+    </div>
+  )
+}
+
 const Pooler = ({ pageContext }) => (
   <Layout>
     <SEO title={pageContext.Equipe} />
     <h1>{pageContext.Equipe}</h1>
     <p>Dirigée par <b>{pageContext.nom}</b> </p>
-    <h2>Alignement actif</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>Joueur</th>
-          <th>Position</th>
-          <th>PJ</th>
-          <th>Buts</th>
-          <th>Aides</th>
-          <th>Score</th>
-          <th>Salaire</th>
-          <th>Date d'ajout</th>
-        </tr>
-      </thead>
-      <tbody>
-        {filtrerAlignement(alignements, pageContext.Id, "Alignement").map(unJoueur => (
-          <tr key={unJoueur.idAlignement}>
-            <td>
-              <Link to={"/joueur/" + unJoueur.idNHL}>
-                {unJoueur.prenomJoueur + " " + unJoueur.nomJoueur}
-              </Link></td>
-            <td>{unJoueur.position} </td>
-            <td>{unJoueur.pjActuels} </td>
-            <td>{unJoueur.butsActuels} </td>
-            <td>{unJoueur.assistActuels} </td>
-            <td>{unJoueur.pointsActuels} </td>
-            <td>{(unJoueur.salaireActuel / 10**6).toFixed(2)  } M$</td>
-            <td>{unJoueur.dateDebut} </td>
-          </tr>
 
-        ))
-        }
-      </tbody>
-    </table>
+    { typesAlignements(pageContext.Id) }
 
-    <h2>Réserve</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>Joueur</th>
-          <th>Position</th>
-          <th>Score</th>
-          <th>Salaire</th>
-          <th>Date d'ajout</th>
-        </tr>
-      </thead>
-      <tbody>
-        {filtrerAlignement(alignements, pageContext.Id, "R\u00e9serve").map(unJoueur => (
-          <tr key={unJoueur.idAlignement}>
-            <td>
-              <Link to={"/joueur/" + unJoueur.idNHL}>
-                {unJoueur.prenomJoueur + " " + unJoueur.nomJoueur}
-              </Link></td>
-            <td>{unJoueur.position} </td>
-            <td>{unJoueur.pointsActuels} </td>
-            <td>{unJoueur.salaireActuel} </td>
-            <td>{unJoueur.dateDebut} </td>
-          </tr>
-
-        ))
-        }
-      </tbody>
-    </table>
   </Layout>
 )
 
